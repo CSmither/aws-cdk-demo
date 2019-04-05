03ff19344ca3bd52ae516acd013d04b080990526
smither <callum@smither.org>
2019-04-05 10:07:41 +0100

    Change ci to build stack only

--- stack/lib/cdkTestStack.ts
+++ stack/lib/cdkTestStack.ts
@@ -1,12 +1,14 @@
 import cdk = require("@aws-cdk/cdk");
-import ecr = require("@aws-cdk/aws-ecr");
 import ecs = require("@aws-cdk/aws-ecs");
 import ec2 = require("@aws-cdk/aws-ec2");
 import { VpcNetwork } from "@aws-cdk/aws-ec2";
+import { DockerImageAsset } from "@aws-cdk/assets-docker";
+import path = require("path");
 
 export class cdkTest extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
     this.node.apply(new cdk.Tag("project", "aws-cdk-test"));
 
     const vpc = new VpcNetwork(this, "MyVPC", { maxAZs: 2 });
@@ -18,11 +20,20 @@ export class cdkTest extends cdk.Stack {
 
     cluster.addCapacity("MyEC2Capacity", {
       instanceType: new ec2.InstanceType("t2.micro"),
-      desiredCapacity: 1
+      desiredCapacity: (new Date().getMinutes() / 10) % 2 === 1 ? 1 : 0,
+      replacingUpdateMinSuccessfulInstancesPercent: 0
     });
 
-    const registry: ecr.Repository = new ecr.Repository(this, "client-repo", {
-      repositoryName: "cdk-demo-client"
+    let clientRepo: string;
+    if (typeof process.env.CLIENT_REPO === "undefined") {
+      throw new Error(
+        "Docker image repo not set. Please set the CLIENT_REPO environment variable"
+      );
+    } else {
+      clientRepo = process.env.CLIENT_REPO;
+    }
+    const asset = new DockerImageAsset(this, "MyBuildImage", {
+      directory: path.join(clientRepo, "client")
     });
 
     const service: ecs.LoadBalancedEc2Service = new ecs.LoadBalancedEc2Service(
@@ -30,9 +41,9 @@ export class cdkTest extends cdk.Stack {
       "ecs-demo-client-service-master",
       {
         cluster: cluster,
-        desiredCount: 1,
-        memoryLimitMiB: 512,
-        image: ecs.ContainerImage.fromEcrRepository(registry, "master"),
+        desiredCount: (new Date().getMinutes() / 10) % 2 === 1 ? 1 : 0,
+        memoryLimitMiB: 128,
+        image: ecs.ContainerImage.fromRegistry(asset.imageUri),
         containerPort: 3000
       }
     );
