646927193850f3fb621068771a1b1f01c441d24d
smither <callum@smither.org>
2019-04-11 17:19:06 +0100

    Add load balancer and service

--- stack/lib/cdkTestStack.ts
+++ stack/lib/cdkTestStack.ts
@@ -1,9 +1,8 @@
 import cdk = require("@aws-cdk/cdk");
 import ecs = require("@aws-cdk/aws-ecs");
 import ec2 = require("@aws-cdk/aws-ec2");
-import { Asset } from "@aws-cdk/assets";
-import { AssetImage, EcrImage } from "@aws-cdk/aws-ecs";
-import { Repository } from "@aws-cdk/aws-ecr";
+import { LoadBalancer } from "@aws-cdk/aws-elasticloadbalancing";
+import { ApplicationLoadBalancer, ApplicationTargetGroup, IpAddressType, ApplicationListener } from "@aws-cdk/aws-elasticloadbalancingv2";
 
 export class cdkTest extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -34,16 +33,39 @@ export class cdkTest extends cdk.Stack {
       clientRepo = process.env.CLIENT_REPO;
     }
 
-    const nameService = new ecs.LoadBalancedEc2Service(this, "name-service", {
+    const taskDefinition = new ecs.Ec2TaskDefinition(this, "TaskDef");
+
+    const container = taskDefinition
+      .addContainer("TheContainer", {
+        image: ecs.ContainerImage.fromAsset(this, "EventImage", {
+          directory: clientRepo
+        }),
+        memoryLimitMiB: 256,
+        logging: new ecs.AwsLogDriver(this, "TaskLogging", {
+          streamPrefix: "EventDemo"
+        })
+      });
+    container.addPortMappings({ containerPort: 3000 });
+
+    const service = new ecs.Ec2Service(this, "Service", {
       cluster: cluster,
-      desiredCount: (new Date().getMinutes() / 10) % 2 === 1 ? 1 : 0,
-      image: EcrImage.fromAsset(this, "image", { directory: clientRepo }),
-      memoryLimitMiB: 128,
-      containerPort: 3000
+      taskDefinition: taskDefinition,
+      desiredCount: 1,
+      minimumHealthyPercent: 0,
+      maximumPercent: 100,
+      placeOnDistinctInstances: false
     });
 
+    const lb : ApplicationLoadBalancer = new ApplicationLoadBalancer(this, "lb", {vpc: vpc, ipAddressType: IpAddressType.Ipv4, internetFacing: true})
+
+    const targetGroup = new ApplicationTargetGroup(this,"targetGroup",{vpc:vpc, port:80})
+
+    const listener: ApplicationListener = lb.addListener("listener", {port: 80, open:true, defaultTargetGroups: [targetGroup]})
+
+    service.attachToApplicationTargetGroup(targetGroup)
+
     new cdk.CfnOutput(this, "LoadBalancerDNS", {
-      value: nameService.loadBalancer.dnsName
+      value: lb.dnsName
     });
   }
 }
